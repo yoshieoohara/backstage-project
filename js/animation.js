9f968c156afd389b164cba0c00d5f26623a544ac
yoshieoohara <you@example.com>
2025-03-19 16:27:18 +0900

    編集

--- js/animation.js
+++ js/animation.js
@@ -11,27 +11,40 @@
 //     document.querySelector(".top-container").style.transform = "translateX(0)";
 //   }
 // });
+// top-line-animation
+gsap.registerPlugin(ScrollTrigger);
 
-// line-animation
+gsap.to(".top-message__ttl--en span::after", {
+  scrollTrigger: {
+    trigger: ".top-message__ttl--en span",
+    start: "top 80%", // ビューポートの80%の位置で開始
+    toggleActions: "play none none none",
+  },
+  width: "100%",
+  duration: 1,
+  ease: "power2.out",
+});
+
+// service-animation
 jQuery(function ($) {
   gsap.registerPlugin(ScrollTrigger);
 
-  const lines = gsap.utils.toArray('.service-message__link-line');
+  const lines = gsap.utils.toArray(".service-message__link-line");
 
   lines.forEach((line, i) => {
-    const arrow = line.querySelector('.arrow');
+    const arrow = line.querySelector(".arrow");
 
     // ラインのアニメーション
     gsap.to(line, {
       scaleX: 1,
-      transformOrigin: 'left center',
-      ease: 'power2.out',
+      transformOrigin: "left center",
+      ease: "power2.out",
       duration: 1,
       delay: i * 0.8, // 上下で時間差
       scrollTrigger: {
         trigger: line,
-        start: 'top 80%',
-        toggleActions: 'play none none none',
+        start: "top 80%",
+        toggleActions: "play none none none",
       },
     });
 
@@ -40,13 +53,12 @@ jQuery(function ($) {
       opacity: 1,
       delay: i * 0.4 + 0.8, // ラインより少し遅らせる
       duration: 0.5,
-      ease: 'power2.out',
+      ease: "power2.out",
       scrollTrigger: {
         trigger: line,
-        start: 'top 80%',
-        toggleActions: 'play none none none',
+        start: "top 80%",
+        toggleActions: "play none none none",
       },
     });
   });
 });
-
