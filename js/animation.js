d37301370ab43092b872fad1d7d2103e14d49354
yoshieoohara <you@example.com>
2025-03-22 16:16:18 +0900

    03.22編集

--- js/animation.js
+++ js/animation.js
@@ -115,3 +115,59 @@ jQuery(function ($) {
     });
   });
 });
+
+// fade-animation
+function fadeIn() {
+  $(".fadeUpTrigger").each(function () {
+    let scroll = $(window).scrollTop();
+    let triTop = $(this).offset().top + 200;
+    let winHeight = $(window).height();
+    if (scroll >= triTop - winHeight) {
+      $(this).addClass("fadeUp");
+    } else {
+      $(this).removeClass("fadeUp");
+    }
+  });
+
+  $(".fadeInTrigger").each(function () {
+    let scroll = $(window).scrollTop();
+    let triTop = $(this).offset().top + 200;
+    let winHeight = $(window).height();
+    if (scroll >= triTop - winHeight) {
+      $(this).addClass("fadeIn");
+    } else {
+      $(this).removeClass("fadeIn");
+    }
+  });
+}
+
+
+$('.fadeLeftTrigger').each(function () {
+  let scroll = $(window).scrollTop();
+  let triTop = $(this).offset().top + 100;
+  let winHeight = $(window).height();
+  if (scroll >= triTop - winHeight) {
+    $(this).addClass('fadeLeft');
+  } else {
+    $(this).removeClass('fadeLeft');
+  }
+});
+
+$('.fadeRightTrigger').each(function () {
+  let scroll = $(window).scrollTop();
+  let triTop = $(this).offset().top + 100;
+  let winHeight = $(window).height();
+  if (scroll >= triTop - winHeight) {
+    $(this).addClass('fadeRight');
+  } else {
+    $(this).removeClass('fadeRight');
+  }
+});
+
+$(window).scroll(function () {
+  fadeIn();
+});
+
+$(document).ready(function () {
+  fadeIn();
+});
