e355f28160c8567b113b2c186f480e8808e24cb1
yoshieoohara <you@example.com>
2025-03-22 16:04:26 +0900

    03.22編集

--- js/accordion.js
+++ js/accordion.js
@@ -1,54 +1,61 @@
- // drawer-accordion
- jQuery(function ($) {
-  $('.js-drawer-accordion').on('click', function () {
+// drawer-accordion
+jQuery(function ($) {
+  $(".js-drawer-accordion").on("click", function () {
     $(this).next().slideToggle();
-    $(this).toggleClass('is-open');
+    $(this).toggleClass("is-open");
   });
-  $('.accordion p').on('click', function () {
-    $(this).toggleClass('is-open');
+  $(".accordion p").on("click", function () {
+    $(this).toggleClass("is-open");
   });
 });
 
- // works-accordion
- document.addEventListener('DOMContentLoaded', function() {
-   const accordionBtns = document.querySelectorAll('.accordion__btn');
+// works-accordion
+document.addEventListener("DOMContentLoaded", function () {
+  const accordionBtns = document.querySelectorAll(".accordion__btn");
 
-   accordionBtns.forEach(btn => {
-     btn.addEventListener('click', function() {
-       const top = btn.closest('.works__list-top');
-       const bottom = top.nextElementSibling; // works__list-bottom
-       const allItems = document.querySelectorAll('.works__list-item'); // 全てのアイテム
+  accordionBtns.forEach((btn) => {
+    btn.addEventListener("click", function () {
+      const item = btn.closest(".works__list-item");
+      const bottom = item.querySelector(".works__list-bottom");
 
-       // 他のアイテムが開いていたら閉じる
-       allItems.forEach(item => {
-         if (item !== top.closest('.works__list-item')) {
-           item.classList.remove('is-open'); // 他のアイテムを閉じる
-           item.querySelector('.works__list-bottom').style.maxHeight = 0; // 高さを0に
-           item.querySelector('.works__list-bottom').style.opacity = 0; // 透明に
-         }
-       });
+      document.querySelectorAll(".works__list-item").forEach((otherItem) => {
+        if (otherItem !== item) {
+          otherItem.classList.remove("is-open");
+          const otherBottom = otherItem.querySelector(".works__list-bottom");
+          if (otherBottom) {
+            otherBottom.style.maxHeight = 0;
+            otherBottom.style.opacity = 0;
+          }
+        }
+      });
 
-       // 開閉のトグル
-       top.classList.toggle('is-open'); // 開く／閉じる
+      item.classList.toggle("is-open");
 
-       // 開いた時の処理
-       if (top.classList.contains('is-open')) {
-         bottom.style.maxHeight = bottom.scrollHeight + 'px'; // コンテンツに合わせた高さを設定
-         bottom.style.opacity = 1;
-       } else {
-         bottom.style.maxHeight = 0;
-         bottom.style.opacity = 0;
-       }
-     });
-   });
- });
+      if (item.classList.contains("is-open")) {
+        bottom.style.maxHeight = bottom.scrollHeight + "px";
+        bottom.style.opacity = 1;
+      } else {
+        bottom.style.maxHeight = 0;
+        bottom.style.opacity = 0;
+      }
+    });
+  });
+});
 
 // artist-accordion
 $(function () {
-  $(".js-accordion-ttl").first().addClass("is-open").next(".js-accordion-txt").show();
+  $(".js-accordion-ttl")
+    .first()
+    .addClass("is-open")
+    .next(".js-accordion-txt")
+    .show();
 
   $(".js-accordion-ttl").click(function () {
-    $(".js-accordion-ttl").not(this).removeClass("is-open").next(".js-accordion-txt").slideUp();
+    $(".js-accordion-ttl")
+      .not(this)
+      .removeClass("is-open")
+      .next(".js-accordion-txt")
+      .slideUp();
 
     $(this).toggleClass("is-open").next(".js-accordion-txt").slideToggle();
   });
